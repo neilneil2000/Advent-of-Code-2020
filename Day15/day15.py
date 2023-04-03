f4277a7b6cbeb72a59490bc5e3283f5d3f0c4025
Neil Jeffery <neil.jeffery@bt.com>
2023-04-03 16:56:24 +0100

    Day 15 Part 2

--- Day15/day15.py
+++ Day15/day15.py
@@ -1,16 +1,17 @@
-TEST_INPUT = [0, 3, 6]
+TEST_INPUT = [3, 1, 2]
 PUZZLE_INPUT = [6, 4, 12, 1, 20, 0, 16]
 
 
 def main():
     starting_numbers = PUZZLE_INPUT
+    number_of_rounds = 30_000_000
     game = {
         number: position + 1
         for position, number in enumerate(starting_numbers)
         if position < len(starting_numbers) - 1
     }
     say_next = starting_numbers[-1]
-    for turn in range(len(starting_numbers), 2020 + 1):
+    for turn in range(len(starting_numbers), number_of_rounds + 1):
         if game.get(say_next) is None:
             game[say_next] = turn
             say_next = 0
@@ -19,7 +20,7 @@ def main():
         game[say_next] = turn
         say_next = say_next_temp
     for key, value in game.items():
-        if value == 2020:
+        if value == number_of_rounds:
             print(key)
 
 
